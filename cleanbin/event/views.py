c902e8fc3ef62e436a5b76be3e48576e56bfa5f7
sab <sab@localhost.localdomain>
2014-10-03 15:50:25 +0530

    adding user event list

--- cleanbin/event/views.py
+++ cleanbin/event/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.base import View
@@ -13,7 +13,6 @@ from event.forms import *
 class EventCreate(CreateView):
     model = Event
     form_class = AddEventForm
-
     
     def form_valid(self, form):
         obj = form.save(commit = False)
@@ -28,7 +27,8 @@ class EventList(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(EventList, self).get_context_data(*args, **kwargs)
-        context['user_events'] = [e.id for e in self.request.user.events.all()]
+        if self.request.user.is_authenticated():
+            context['user_events'] = [e.id for e in self.request.user.events.all()]
         return context
 
 class EventDetail(DetailView):
@@ -37,11 +37,12 @@ class EventDetail(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(EventDetail, self).get_context_data(*args, **kwargs)
-        user_events = [e.id for e in self.request.user.events.all()]
-        if context['event'].id in user_events:
-            context['participating'] = True
-        else:
-            context['participating'] = False
+        if self.request.user.is_authenticated():
+            user_events = [e.id for e in self.request.user.events.all()]
+            if context['event'].id in user_events:
+                context['participating'] = True
+            else:
+                context['participating'] = False
         return context
 
 class JoinView(View):
@@ -51,3 +52,21 @@ class JoinView(View):
         messages.add_message(request, messages.INFO, "You are added as a participant")
         return redirect(reverse('list-event'))
 
+class UnJoinView(View):
+    def get(self, request, *args, **kwargs):
+        self.event = get_object_or_404(Event, pk=kwargs.get('pk'))
+        self.event.participants.remove(request.user)
+        messages.add_message(request, messages.INFO, "You are removed from the event :(")
+        return redirect(reverse('my-events'))
+
+
+class UserEventList(EventList):
+    template_name = "event/user_event_list.html"
+
+    def get_queryset(self):
+        return self.request.user.events.all()
+
+class EventUpdate(UpdateView):
+    model = Event
+    form_class = AddEventForm
+    template_name = 'event_update_form.html'
